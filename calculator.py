656ed07d1891fd5fad04761a503e5df1e2f95308
Shreyansh <69718041+Shreyansh2006@users.noreply.github.com>
2020-10-02 17:35:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
 import time
+d = input("y or n")
 Userinput1 = float(input("Enter The First Number :\n"))
 Userinput2 = float(input("Enter The Second Number :\n"))
 bodmas = str(input("Enter what you want to do ADD, SUBTRACT, MULTIPLICATION and DIVISION\npress +, -, * or /..."))
 
-
-if bodmas=="+":
-    print(f"The Sum is {Userinput1 + Userinput2}")
-elif bodmas=="-":
-    print(f"The Difference is {Userinput1 - Userinput2}")
-elif bodmas=="*":
-    print(f"The Product is {Userinput1 * Userinput2}")
-elif bodmas=="/":
-    print(f"The Quotient is {Userinput1 / Userinput2}")
-time.sleep(20)
+if d == "y":
+    if bodmas=="+":
+        print(f"The Sum is {Userinput1 + Userinput2}")
+    elif bodmas=="-":
+        print(f"The Difference is {Userinput1 - Userinput2}")
+    elif bodmas=="*":
+        print(f"The Product is {Userinput1 * Userinput2}")
+    elif bodmas=="/":
+        print(f"The Quotient is {Userinput1 / Userinput2}")
+    time.sleep(20)
+else:
+    exit()
